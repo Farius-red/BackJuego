2f7d59349b2dd631225c79c06889e52d5a1fd627
unknown <zigmainflables@gmail.com>
2021-05-25 17:19:35 -0500

    esta restringiendo la edicion de usuario

--- auth/index.js
+++ auth/index.js
@@ -1,10 +1,44 @@
 const jwt = require('jsonwebtoken');
+const config = require('./config');
+const secreto = config.jwt.secret;
 
 function sign(data){
-     return jwt.sign(data, 'secreto');
+     return jwt.sign(data, secreto);
 }
 
+const check = {
+  own: function(req, owner){
+    const decoded = decodeHeader(req);
+    console.log(decoded);
+  }
+}
+
+function verify(token){
+    return jwt.verify(token, secreto);
+}
+function getToken(auth){
+    if(!auth){
+        throw new Error('no llego token');
+    }
+
+    if(auth.indexOf('Bearer') === -1){
+        throw new Error('formato indeseado del token');
+    }
+    let token = auth.replace('Bearer', '' );
+    return token;
+}
+
+function decodeHeader(req){
+        const authorization = req.headers.authorization || '';
+        const token = getToken(authorization);
+        const decoded = verify(token);
+
+        req.user = decoded;
+
+        return decoded;
+}
 
 module.exports ={
  sign,
+ check
 };
--- config.js
+++ config.js
@@ -1,5 +1,9 @@
 module.exports = {
     api: {
         port: process.env.API_PORT || 3002,
+    },
+
+    jwt: {
+        secret: process.env.JWT_SECRET || 'notasecreto',
     }
 }
